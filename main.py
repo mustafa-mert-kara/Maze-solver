a62a499cf203cb0d82e090ae58bee803f3c5b56f
Mustafa Mert Kara <mustafa.m.kara97@gmail.com>
2025-03-06 12:12:09 +0300

    Added window class and main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from window import Window
+
+
+def main():
+    print("Game is Beginning")
+    game_window=Window(1200,800)
+    game_window.wait_for_close()
+
+
+
+
+if __name__=="__main__":
+    main()
---
+++ window.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window():
+    def __init__(self,width,height):
+        self.__root=Tk()
+        self.__root.title="Maze Solver"
+        self.__canvas=Canvas()
+        self.__canvas.pack()
+        self.is_running=False
+
+        self.__root.minsize(width,height)
+        self.__root.maxsize(width,height)
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.is_running=True
+        while self.is_running:
+            self.redraw()
+    def close(self):
+        self.is_running=False
